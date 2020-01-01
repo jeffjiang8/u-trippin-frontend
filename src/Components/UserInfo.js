0c34613e4ba645f18ec0197c10add3047788a279
Jeff Jiang <jeffjiang8@gmail.com>
2020-01-01 16:15:58 -0500

    installed inline editor

--- src/Components/UserInfo.js
+++ src/Components/UserInfo.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react';
+// import InlineEdit from 'react-edit-inline';
 
 class UserInfo extends Component {
 
     state = {
-        user: null
+        user: null,
+        username: '',
+        firstName: '',
+        lastName: '',
+        age: '',
+        email: '',
+        phoneNumber: '',
     }
 
     componentDidMount(){
@@ -14,11 +21,34 @@ class UserInfo extends Component {
         }))
     }
 
+    handleChange = (e)=>{
+        let name = e.target.name
+        let value = e.target.value
+        this.setState({
+            [name]: value
+        })
+    }
+
     render() {
-        console.log(this.state.user)
         return (
             <div className="user-account">
-                <h1>hi</h1>
+                {   this.state.user === null
+                    ?
+                    ""
+                    :
+                    <>
+                        <h2>Usename: {this.state.user.username}</h2>
+                        <h2>First Name: {this.state.user.first_name}</h2>
+                        <h2>Last Name: {this.state.user.last_name}</h2>
+                        <h2>Age: {this.state.user.age}</h2>
+                        <h2>Email: {this.state.user.email}</h2>
+                        <h2>Phone Number: {this.state.user.phone_number}</h2>
+                        <div className="user-account-action">
+                            <button>Save</button>
+                            <button>Delete Account</button>
+                        </div>
+                    </>
+                }
             </div>
         );
     }
